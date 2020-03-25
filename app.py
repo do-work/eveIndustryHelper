27a5f510133fffd65e8835792d9711064cdc9cae
David Olson <mugency@gmail.com>
2020-03-24 23:47:50 -0400

    1#card-35047061: Add testing endpoint.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify
 from flask import request
 
 from controllers.oauth import Oauth
+from settings import LOGIN_SERVER_BASE
 
 app = Flask(__name__)
 
@@ -13,12 +15,22 @@ def index():
 
 @app.route("/oauth/authorize", methods=["GET"])
 def authorize():
-    Oauth().authorize()
+    # todo -> add redirect
+    return Oauth().get_authorize_endpoint()
 
 
 @app.route("/oauth-callback", methods=["GET"])
 def callback():
-    Oauth().callback(request)
+    return jsonify(Oauth().callback(request))
+
+
+@app.route("/toon_test", methods=["GET"])
+def testing():
+    # temporary endpoint for testing
+    access_token = Oauth().get_refreshed_access_token()
+    headers = {"authorization": f"Bearer {access_token}"}
+    response = requests.get(f"{LOGIN_SERVER_BASE}/oauth/verify", headers=headers)
+    return jsonify(response.json())
 
 
 if __name__ == "__main__":
