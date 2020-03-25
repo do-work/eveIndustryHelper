03f3945b0a587b3f35b3dee4c58239cc0887fa48
David Olson <mugency@gmail.com>
2020-03-24 23:44:53 -0400

    1#card-35047061: Add env vars to settings file.

--- settings.py
+++ settings.py
@@ -1,2 +1,9 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
+
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+LOGIN_SERVER_BASE = os.getenv("LOGIN_SERVER_BASE")
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
