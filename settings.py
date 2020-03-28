4d9230561463a2b64a009deaf57651d6ee5e7ae8
David Olson <david@deolson.com>
2020-03-28 14:23:14 -0400

    1#card-35047113: Add project_root to settings.

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 CLIENT_ID = os.getenv("CLIENT_ID")
 LOGIN_SERVER_BASE = os.getenv("LOGIN_SERVER_BASE")
 REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
